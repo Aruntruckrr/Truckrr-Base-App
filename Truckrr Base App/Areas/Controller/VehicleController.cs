55b608b125b0dee0eda415033235fe307145d50e
Arunachaleswarar M <arun@truckrr.com>
2023-07-21 15:19:27 +0530

    chabges

--- Truckrr Base App/Areas/Controller/VehicleController.cs
+++ Truckrr Base App/Areas/Controller/VehicleController.cs
@@ -1,30 +1,77 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Truckrr_Base_App.Web.Areas.Models;
 
 namespace Truckrr_Base_App.Areas.Controller
 {
     public class VehicleController : ControllerBase
     {
-        private object _mediator;
-        private object _mapper;
-
-        public IActionResult Index()
+        [HttpPost]
+        
+        public Task<ActionResult> AddVehicle(String vehicleId,string VehicleName)
         {
-            return View();
-        }
-        public async Task<IActionResult> LoadAll()
-        {
-            var response = await _mediator.Send(new GetAllVehicleCachedQuery());
-            if (response.Succeeded)
+            try
             {
-                var viewModel = _mapper.Map<List<VehicleModel>>(response.Data);
-                return PartialView("_ViewAll", viewModel);
+                if (vehicleId != null && VehicleName!=null)
+                {
+
+                }
             }
-            return null;
+            catch{
+
+            }
+            return (Task<ActionResult>)View();
+        }
+        public async Task<ActionResult> EditVehicle(String vehicleId)
+        {
+            try
+            {
+
+            }
+            catch
+            {
+
+            }
+            return (ActionResult)View();
+        }
+        public async Task<ActionResult> DeleteVehicle(String vehicleId)
+        {
+            try
+            {
+
+            }
+            catch
+            {
+
+            }
+            return (ActionResult)View();
+        }
+        public async Task<ActionResult> UploadDocument(String vehicleId)
+        {
+            try
+            {
+
+            }
+            catch
+            {
+
+            }
+            return (ActionResult)View();
+        }
+        public async Task<ActionResult> LoadVehicle(String vehicleId)
+        {
+            try
+            {
+
+            }
+            catch
+            {
+
+            }
+            return (ActionResult)View();
         }
 
         private IActionResult PartialView(string v, object viewModel)
         {
+
             throw new NotImplementedException();
         }
 
---
+++ Truckrr Base App/StartupBase.cs
@@ -0,0 +1,26 @@
+﻿namespace Truckrr_Base_App
+{
+    public class StartupBase
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            {
+                //app.UseNotyf();
+                app.UseHttpsRedirection();
+                app.UseStaticFiles();
+                app.UseCookiePolicy();
+                //app.UseMultiLingualFeature();
+                app.UseRouting();
+                app.UseAuthentication();
+                app.UseAuthorization();
+                app.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapRazorPages();
+                    endpoints.MapControllerRoute(
+                        name: "default",
+                        pattern: "{area=Dashboard}/{controller=Home}/{action=Index}/{id?}");
+                });
+            }
+        }
+    }
+}
